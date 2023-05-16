f17081e39e8e3e3cc77836f55550fb7ebaf2211a
Gibok <sgl7955@gmail.com>
2023-05-16 14:32:19 +0900

    화폐단위 위치 수정

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">$${mov}</div>
+        <div class="movements__value">${mov}$</div>
       </div>
         `;
 
