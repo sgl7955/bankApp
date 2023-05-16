c5e96b5843cc6513f40503bea452119aa7bafb07
Gibok <sgl7955@gmail.com>
2023-05-16 14:23:38 +0900

    화폐단위 위치 수정

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}$</div>
+        <div class="movements__value">$${mov}</div>
       </div>
         `;
 
@@ -85,19 +85,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}$`;
+  labelBalance.textContent = `$${acc.balance}`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}$`;
+  labelSumIn.textContent = `$${incomes}`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}$`;
+  labelSumOut.textContent = `$${Math.abs(out)}`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -106,7 +106,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}$`;
+  labelSumInterest.textContent = `$${interest.toFixed(2)}`;
 };
 
 const createUsernames = function (accs) {
